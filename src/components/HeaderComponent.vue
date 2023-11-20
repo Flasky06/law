a62af1f420391b0c285a2486e9f303bf4e12ebed
WACHIURI001 <nestawriter@gmail.com>
2023-11-20 18:09:42 +0300

    attoney

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,99 +1,139 @@
 <template>
-	<div class="bg-primary top-0 sticky z-40">
-		<div
-			class="hidden lg:flex lg:flex-row lg:justify-center lg:py-3 lg:space-x-24 bg-[#1E1E1E] text-white border-b-orange-500"
-		>
-			<div><span>Call Us</span><span>+254 712 345 678</span></div>
+  <div class="bg-primary top-0 sticky z-40">
+    <!-- <div
+      class="hidden lg:flex lg:flex-row lg:justify-center lg:py-3 lg:space-x-24 bg-[#1E1E1E] text-white border-b-orange-500"
+    >
+      <div><span>Call Us</span><span>+254 712 345 678</span></div>
 
-			<div><span>tw</span><span>ig</span><span>fb</span></div>
-		</div>
-		<!-- Desktop Navbar -->
-		<div
-			class="px-12 sm:px-6 hidden lg:flex justify-between lg:items-center lg:py-8 bg-secondary"
-		>
-			<router-link class="text-white text-2xl" to="/"> LAWYER </router-link>
+      <div><span>tw</span><span>ig</span><span>fb</span></div>
+    </div> -->
+    <div
+      class="sub-header py-2 text-sm flex flex-col lg:flex-row lg:justify-around items-center bg-black"
+    >
+      <!-- Phone number section -->
+      <div class="phone mb-2 lg:mb-0">
+        <p class="text-center">
+          Call us: <span class="text-yellow-600">(+254)719653260</span>
+        </p>
+      </div>
+      <!-- Mail section -->
+      <div class="mail flex items-center lg:ml-4">
+        <p class="text-yellow-600 ml-1 lg:ml-2">info@companyname.com</p>
+      </div>
+      <!-- Social media icons section -->
+      <section class="flex social-icons mt-2 lg:mt-0">
+        <img
+          class="w-10 mr-4"
+          src="https://i.pinimg.com/564x/2f/b9/09/2fb909b74a854f0715e64dda5825990d.jpg"
+          alt="Social Media Icon"
+        />
+        <img
+          class="w-10 mr-4"
+          src="https://i.pinimg.com/564x/2f/b9/09/2fb909b74a854f0715e64dda5825990d.jpg"
+          alt="Social Media Icon"
+        />
+        <img
+          class="w-10"
+          src="https://i.pinimg.com/564x/2f/b9/09/2fb909b74a854f0715e64dda5825990d.jpg"
+          alt="Social Media Icon"
+        />
+      </section>
+    </div>
+    <div class="border-t border-yellow-600"></div>
+    <!-- End of sub header -->
+    <!-- Desktop Navbar -->
+    <div
+      class="px-12 sm:px-6 hidden lg:flex justify-between lg:items-center lg:py-8 bg-secondary"
+    >
+      <router-link class="text-white text-4xl" to="/">
+        LAW<span class="text-text_color">YER</span>
+      </router-link>
 
-			<ul class="space-x-16 lg:flex mr-20">
-				<li><router-link to="/" class="text-white">HOME</router-link></li>
-				<li>
-					<router-link
-						to="/practice-area"
-						@click="toggleMobileSidebar"
-						class="text-white"
-						>PRACTICE AREA</router-link
-					>
-				</li>
-				<li>
-					<router-link
-						to="/attorneys"
-						@click="toggleMobileSidebar"
-						class="text-white"
-					>
-						ATTORNEYS
-					</router-link>
-				</li>
-				<li>
-					<router-link
-						to="/contact-us"
-						@click="toggleMobileSidebar"
-						class="text-white"
-						>CONTACT US</router-link
-					>
-				</li>
-			</ul>
-		</div>
+      <ul class="space-x-16 lg:flex mr-20">
+        <li>
+          <router-link to="/" class="text-white hover:text-text_color"
+            >HOME</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            to="/practice-area"
+            @click="toggleMobileSidebar"
+            class="text-white hover:text-text_color"
+            >PRACTICE AREA</router-link
+          >
+        </li>
+        <li>
+          <router-link
+            to="/attorneys"
+            @click="toggleMobileSidebar"
+            class="text-white hover:text-text_color"
+          >
+            ATTORNEYS
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            to="/contact-us"
+            @click="toggleMobileSidebar"
+            class="text-white hover:text-text_color"
+            >CONTACT US</router-link
+          >
+        </li>
+      </ul>
+    </div>
 
-		<!-- Mobile Navbar (sidebar) -->
-		<div
-			class="lg:hidden fixed w-full h-20 z-[100] shadow-xl transition-transform ease-in-out duration-300 bg-slate-950"
-		>
-			<div class="max-w-7xl lg:px-8 py-4">
-				<div class="flex items-center justify-between">
-					<router-link to="/">
-						<span class="text-white text-xl ml-4"> Infospace</span>
-					</router-link>
-					<button
-						@click="toggleMobileSidebar"
-						class="text-white mr-2 text-2xl lg:hidden"
-					>
-						☰
-					</button>
-				</div>
-				<div name="slide " class="w-9/12 ml-auto left-0">
-					<div
-						v-if="mobileSidebarOpen"
-						class="bg-[#061158] text-white mt-4 p-4 rounded-lg opacity-80 h-screen"
-					>
-						<div class="text-end mt-4 text-2xl">
-							<button @click="toggleMobileSidebar" class="text-white">X</button>
-						</div>
-						<ul class="space-y-12 text-xl ml-4">
-							<li>
-								<router-link to="/" @click="toggleMobileSidebar">
-									Home</router-link
-								>
-							</li>
-							<li>
-								<router-link to="/practice-area" @click="toggleMobileSidebar">
-									About</router-link
-								>
-							</li>
-							<li>
-								<router-link to="/attorneys" @click="toggleMobileSidebar">
-									Services</router-link
-								>
-							</li>
-							<li>
-								<router-link to="/contact-us" @click="toggleMobileSidebar">
-									Portfolio</router-link
-								>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <!-- Mobile Navbar (sidebar) -->
+    <div
+      class="lg:hidden fixed w-full h-20 z-[100] shadow-xl transition-transform ease-in-out duration-300 bg-slate-950"
+    >
+      <div class="max-w-7xl lg:px-8 py-4">
+        <div class="flex items-center justify-between">
+          <router-link to="/">
+            <span class="text-white text-xl ml-4"> Infospace</span>
+          </router-link>
+          <button
+            @click="toggleMobileSidebar"
+            class="text-white mr-2 text-2xl lg:hidden"
+          >
+            ☰
+          </button>
+        </div>
+        <div name="slide " class="w-9/12 ml-auto left-0">
+          <div
+            v-if="mobileSidebarOpen"
+            class="bg-[#061158] text-white mt-4 p-4 rounded-lg opacity-80 h-screen"
+          >
+            <div class="text-end mt-4 text-2xl">
+              <button @click="toggleMobileSidebar" class="text-white">X</button>
+            </div>
+            <ul class="space-y-12 text-xl ml-4">
+              <li>
+                <router-link to="/" @click="toggleMobileSidebar">
+                  Home</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/practice-area" @click="toggleMobileSidebar">
+                  About</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/attorneys" @click="toggleMobileSidebar">
+                  Services</router-link
+                >
+              </li>
+              <li>
+                <router-link to="/contact-us" @click="toggleMobileSidebar">
+                  Portfolio</router-link
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
 import { ref } from "vue";
@@ -101,17 +141,17 @@ import { ref } from "vue";
 const mobileSidebarOpen = ref(false);
 
 const toggleMobileSidebar = () => {
-	mobileSidebarOpen.value = !mobileSidebarOpen.value;
+  mobileSidebarOpen.value = !mobileSidebarOpen.value;
 };
 </script>
 
 <style>
 .slide-enter-active,
 .slide-leave-active {
-	transition: transform 0.3s;
+  transition: transform 0.3s;
 }
 .slide-enter,
 .slide-leave-to {
-	transform: translateX(100%);
+  transform: translateX(100%);
 }
 </style>
--- src/views/Attorneys.vue
+++ src/views/Attorneys.vue
@@ -1,5 +1,300 @@
 <template>
-	<h1 class="text-green">Artoneys</h1>
+  <div class="mt-16">
+    <div
+      class="header py-8 text-slate-200 bg-black flex items-center justify-around"
+    >
+      <section><h1 class="text-xl">Attorney</h1></section>
+      <section class="flex items-center text-sm">
+        <span class="material-symbols-outlined">home</span>
+        <p>> Attorny</p>
+      </section>
+    </div>
+    <div class="holder bg-primary py-8">
+      <div
+        class="grid grid-col-1 p-6 md:space-y-0 md:grid-cols-2 gap-4 md:py-2 lg:max-w-7xl mx-auto lg:grid-cols-4 lg:gap-4 lg:space-y-0"
+      >
+        <!--card 1-->
+        <div class="text-white bg-secondary p-2">
+          <div class="card-img h-48 overflow-hidden">
+            <img
+              src="https://images.pexels.com/photos/8729729/pexels-photo-8729729.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt="Card 1"
+              class="w-full h-full object-cover"
+            />
+          </div>
+
+          <div class="titles flex flex-col space-y-4">
+            <h2 class="text-lg font-bold mt-2">Kibaki Maingi</h2>
+            <h3 class="text-sm text-text_color">Civil Rights Law</h3>
+          </div>
+          <div class="border-t border-slate-200 my-4 m-auto"></div>
+          <p class="text-base">
+            Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+            Praesent mauris fusce nec.
+          </p>
+          <div class="social-logos flex items-center space-x-2 py-2">
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/fb.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ig.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/lin.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ws.png" />
+            </div>
+          </div>
+        </div>
+
+        <!--card 2-->
+        <div class="text-white bg-secondary p-2">
+          <div class="card-img h-48 overflow-hidden">
+            <img
+              src="https://images.pexels.com/photos/5648409/pexels-photo-5648409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Card 1"
+              class="w-full h-full object-cover"
+            />
+          </div>
+          <div class="titles flex flex-col space-y-4">
+            <h2 class="text-lg font-bold mt-2">Peris Maingi</h2>
+            <h3 class="text-sm text-text_color">Commercial Law</h3>
+          </div>
+
+          <div class="border-t border-slate-200 my-4 m-auto"></div>
+          <p class="text-base">
+            Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+            Praesent mauris fusce nec.
+          </p>
+          <div class="social-logos flex items-center space-x-2 py-2">
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/fb.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ig.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/lin.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ws.png" />
+            </div>
+          </div>
+        </div>
+
+        <!--card 3-->
+        <div class="text-white bg-secondary p-2">
+          <div class="card-img h-48 overflow-hidden">
+            <img
+              src="https://images.pexels.com/photos/8112114/pexels-photo-8112114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Card 1"
+              class="w-full h-full object-cover"
+            />
+          </div>
+
+          <div class="titles flex flex-col space-y-4">
+            <h2 class="text-lg font-bold mt-2">Karius Mushemi</h2>
+            <h3 class="text-sm text-text_color">Criminal Law</h3>
+          </div>
+          <div class="border-t border-slate-200 my-4 m-auto"></div>
+          <p class="text-base">
+            Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+            Praesent mauris fusce nec.
+          </p>
+          <div class="social-logos flex items-center space-x-2 py-2">
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/fb.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ig.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/lin.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ws.png" />
+            </div>
+          </div>
+        </div>
+
+        <!--card 4-->
+        <div class="text-white bg-secondary p-2">
+          <div class="card-img h-48 overflow-hidden">
+            <img
+              src="https://images.pexels.com/photos/5313181/pexels-photo-5313181.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt="Card 1"
+              class="w-full h-full object-cover"
+            />
+          </div>
+
+          <div class="titles flex flex-col space-y-4">
+            <h2 class="text-lg font-bold mt-2">Klamis Onfigo</h2>
+            <h3 class="text-sm text-text_color">Coporate Law</h3>
+          </div>
+          <div class="border-t border-slate-200 my-4 m-auto"></div>
+          <p class="text-base">
+            Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+            Praesent mauris fusce nec.
+          </p>
+          <div class="social-logos flex items-center space-x-2 py-2">
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/fb.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ig.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/lin.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ws.png" />
+            </div>
+          </div>
+        </div>
+
+        <!--card 5-->
+        <div class="text-white bg-secondary p-2">
+          <div class="card-img h-48 overflow-hidden">
+            <img
+              src="https://images.pexels.com/photos/5668766/pexels-photo-5668766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Card 1"
+              class="w-full h-full object-cover"
+            />
+          </div>
+
+          <div class="titles flex flex-col space-y-4">
+            <h2 class="text-lg font-bold mt-2">Askaris Luis</h2>
+            <h3 class="text-sm text-text_color">Finance Law</h3>
+          </div>
+          <div class="border-t border-slate-200 my-4 m-auto"></div>
+          <p class="text-base">
+            Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+            Praesent mauris fusce nec.
+          </p>
+          <div class="social-logos flex items-center space-x-2 py-2">
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/fb.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ig.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/lin.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ws.png" />
+            </div>
+          </div>
+        </div>
+
+        <!--card 6-->
+
+        <div class="text-white bg-secondary p-2">
+          <div class="card-img h-48 overflow-hidden">
+            <img
+              src="https://images.pexels.com/photos/8730760/pexels-photo-8730760.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt="Card 1"
+              class="w-full h-full object-cover"
+            />
+          </div>
+
+          <div class="titles flex flex-col space-y-4">
+            <h2 class="text-lg font-bold mt-2">Gitmus Ofuadi</h2>
+            <h3 class="text-sm text-text_color">Criminal Law</h3>
+          </div>
+          <div class="border-t border-slate-200 my-4 m-auto"></div>
+          <p class="text-base">
+            Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+            Praesent mauris fusce nec.
+          </p>
+          <div class="social-logos flex items-center space-x-2 py-2">
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/fb.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ig.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/lin.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ws.png" />
+            </div>
+          </div>
+        </div>
+
+        <!--card 7-->
+        <div class="text-white bg-secondary p-2">
+          <div class="card-img h-48 overflow-hidden">
+            <img
+              src="https://images.pexels.com/photos/7875836/pexels-photo-7875836.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt="Card 1"
+              class="w-full h-full object-cover"
+            />
+          </div>
+
+          <div class="titles flex flex-col space-y-4">
+            <h2 class="text-lg font-bold mt-2">Roor Ikisogo</h2>
+            <h3 class="text-sm text-text_color">Family Law</h3>
+          </div>
+          <div class="border-t border-slate-200 my-4 m-auto"></div>
+          <p class="text-base">
+            Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+            Praesent mauris fusce nec.
+          </p>
+          <div class="social-logos flex items-center space-x-2 py-2">
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/fb.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ig.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/lin.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ws.png" />
+            </div>
+          </div>
+        </div>
+        <!--card 8-->
+        <div class="text-white bg-secondary p-2">
+          <div class="card-img h-48 overflow-hidden">
+            <img
+              src="https://images.pexels.com/photos/4427622/pexels-photo-4427622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Card 1"
+              class="w-full h-full object-cover"
+            />
+          </div>
+
+          <div class="titles flex flex-col space-y-4">
+            <h2 class="text-lg font-bold mt-2">Kifua Kifundi</h2>
+            <h3 class="text-sm text-text_color">Consumer Law</h3>
+          </div>
+          <div class="border-t border-slate-200 my-4 m-auto"></div>
+          <p class="text-base">
+            Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+            Praesent mauris fusce nec.
+          </p>
+          <div class="social-logos flex items-center space-x-2 py-2">
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/fb.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ig.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/lin.png" />
+            </div>
+            <div class="border border-slate-200 p-1">
+              <img src="/resources/social-media-logos/ws.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup></script>
 <style></style>
