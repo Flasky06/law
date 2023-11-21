e407804974b0ede1762b1621af8ef59219d61870
Wambuku <julietwambuku361@gmail.com>
2023-11-21 12:58:14 +0300

    message

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="w-full h-[1150px] lg:h-[440px] bg-green-950">
-      <div class="lg:px-80 lg:flex justify-between">
-        <div class="grid grid-col-1 gap-8 space-y-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4 lg:gap-10">
-          <div class="py-4 space-y-4">
+  <div class="w-full px-6 h-[1300px] md:h-[750px] lg:h-[440px] bg-green-950">
+    <div class="lg:px-80 lg:flex justify-between">
+      <div
+        class="grid grid-col-1 gap-8 space-y-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4 lg:gap-10"
+      >
+        <div class="py-4 space-y-4">
           <h1>
             <span class="font-bold text-orange-500">|</span>&nbsp;
             <span class="text-xl text-white">A</span
@@ -12,14 +14,15 @@
             ><span class="text-white text-sm">S</span>
           </h1>
           <p class="text-white text-start">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-            necessitatibus minus esse ipsum officia? Inventore, rem. Deserunt
-            dolore quibusdam eos nemo labore magni, accusantium, tenetur
-            obcaecati, fugiat cupiditate iure fuga.
+            Attorneys within a law firm work collaboratively on cases, offering
+            their expertise and guidance to individuals, businesses, or
+            organizations seeking legal assistance.
           </p>
           &nbsp;
-          <p class="text-white text-start">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <p
+            class="text-[#B17030] text-start border border-[#166432] w-9/12 text-sm px-2 font-bold"
+          >
+            Fight for justice and peace.
           </p>
         </div>
         <div class="space-y-4">
@@ -35,31 +38,41 @@
             <span class="material-symbols-outlined text-sm">
               radio_button_unchecked
             </span>
-            <a href="/herosView" class="hover:animate-pulse">Auto & Car Accidents</a>
+            <a href="/herosView" class="hover:animate-pulse"
+              >Auto & Car Accidents</a
+            >
           </div>
           <div class="flex space-x-2 text-orange-400">
             <span class="material-symbols-outlined text-sm">
               radio_button_unchecked
             </span>
-            <a href="/herosView" class="hover:animate-pulse">Sexual Abuse and Assault</a>
+            <a href="/herosView" class="hover:animate-pulse"
+              >Sexual Abuse and Assault</a
+            >
           </div>
           <div class="flex space-x-2 text-orange-400">
             <span class="material-symbols-outlined text-sm">
               radio_button_unchecked
             </span>
-            <a href="/herosView" class="hover:animate-pulse">Business Litgation</a>
+            <a href="/herosView" class="hover:animate-pulse"
+              >Business Litgation</a
+            >
           </div>
           <div class="flex space-x-2 text-orange-400">
             <span class="material-symbols-outlined text-sm">
               radio_button_unchecked
             </span>
-            <a href="/herosView" class="hover:animate-pulse">Defective Medical Device</a>
+            <a href="/herosView" class="hover:animate-pulse"
+              >Defective Medical Device</a
+            >
           </div>
           <div class="flex space-x-2 text-orange-400">
             <span class="material-symbols-outlined text-sm">
               radio_button_unchecked
             </span>
-            <a href="/herosView" class="hover:animate-pulse">Toyota Accelerator Recall</a>
+            <a href="/herosView" class="hover:animate-pulse"
+              >Toyota Accelerator Recall</a
+            >
           </div>
           <div class="flex space-x-2 text-orange-400">
             <span class="material-symbols-outlined text-sm">
@@ -67,12 +80,14 @@
             </span>
             <a href="/herosView" class="hover:animate-pulse">Corporate Fund</a>
           </div>
-      
+
           <div class="flex space-x-2 text-orange-400">
             <span class="material-symbols-outlined text-sm">
               radio_button_unchecked
             </span>
-            <a href="/herosView" class="hover:animate-pulse">Auto Product Liability</a>
+            <a href="/herosView" class="hover:animate-pulse"
+              >Auto Product Liability</a
+            >
           </div>
           <div class="flex space-x-2 text-orange-400">
             <span class="material-symbols-outlined text-sm">
@@ -92,30 +107,31 @@
               >I</span
             ><span class="text-white text-sm">NFO</span>
           </h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          <p class="text-white">You can reach us out via our socials.</p>
           <div class="flex space-x-2">
             <span
-              class="material-symbols-outlined border border-orange-400 px-2 py-2"
+              class="material-symbols-outlined text-[#B17030] border border-[#166432] px-2 py-2"
             >
               location_on
             </span>
-            <p>Sydeney,Australia</p>
+            <p class="text-white">Sydeney,Australia</p>
           </div>
           <div class="flex space-x-2">
             <span
-              class="material-symbols-outlined border border-orange-400 px-2 py-2"
+              class="material-symbols-outlined text-[#B17030] border border-[#166432] px-2 py-2"
             >
               call
             </span>
-            <p>+111 22-33-444</p>
+            <p class="text-white">+111 22-33-444</p>
           </div>
           <div class="flex space-x-2">
             <span
-              class="material-symbols-outlined border border-orange-400 px-2 py-2"
+              class="material-symbols-outlined text-[#B17030] border border-[#166432] px-2 py-2"
             >
               mail
             </span>
-            <p>contact@yourlawwpsite.com</p>
+
+            <p class="text-white">contact@yourlawwpsite.com</p>
           </div>
         </div>
         <div>
@@ -124,7 +140,9 @@
             <span class="text-xl text-white">N</span
             ><span class="text-white text-sm">EWSLETTER</span>
           </h1>
-          <p>Follow our legal advice in a form of a monthly newsletter</p>
+          <p class="text-white">
+            Follow our legal advice in a form of a monthly newsletter
+          </p>
           <div class="py-4 px-2 flex">
             <input type="email" placeholder="Enter your email address" />
             <button
@@ -142,44 +160,27 @@
             ><span class="text-white text-sm">S</span>
           </h1>
           <div class="py-6 flex space-x-6">
-            <div>
-              <span
-                class="material-symbols-outlined border border-orange-400 px-2 py-2"
-              >
-                location_on
-              </span>
+            <div class="px-2 py-2 border border-orange-400">
+              <fa
+                :icon="['fab', 'facebook']"
+                class="text-white text-xl cursor-pointer"
+              ></fa>
             </div>
-            <div>
-              <span
-                class="material-symbols-outlined border border-orange-400 px-2 py-2"
-              >
-                location_on
-              </span>
+            <div class="px-2 py-2 border border-orange-400">
+              <fa
+                :icon="['fab', 'instagram']"
+                class="text-white text-xl cursor-pointer"
+              ></fa>
             </div>
-            <div>
-              <span
-                class="material-symbols-outlined border border-orange-400 px-2 py-2"
-              >
-                location_on
-              </span>
+            <div class="px-2 py-2 border border-orange-400">
+              <fa
+                :icon="['fab', 'twitter']"
+                class="text-white text-xl cursor-pointer"
+              ></fa>
             </div>
           </div>
         </div>
       </div>
-      
-      </div>
-      <div class="w-full lg:h-20 bg-black">
-      <div class="flex text-white text-sm px-4 py-6">
-        <span class="material-symbols-outlined"> copyright </span>
-        <p>COPYRIGHT 2023 -CHANGE THE WORLD WITH</p>
-        &nbsp;
-        <span>
-          <p class="text-orange-400">LAW</p>
-        </span>
-      </div>
-      
     </div>
-    </div>
-  
-  </template>
-  
+  </div>
+</template>
